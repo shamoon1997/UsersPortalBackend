3d65a856130015fc973490ab5b1c7fa95ef25279
shamoon1997 <sshamoon83@gmail.com>
2023-09-14 11:16:34 +0500

    feat: code improved

--- index.js
+++ index.js
@@ -20,24 +20,58 @@ db.connect((err) => {
 
 const app = express();
 const port = process.env.PORT || 9001;
+let searchDataForOtherQuery;
 
 app.use(cors());
 // Define a GET endpoint for reading data from the database
 app.get('/api/data', (req, res) => {
   const page = req.query.page || 1; // Get the page number from the query parameter
-  const searchValue = req.query.searchValue || undefined;
-  const searchColumn = req.query.searchColumn || undefined;
+  let searchData = req.query.searchValue || undefined;
+  searchDataForOtherQuery = searchData || undefined;
 
   const pageSize = 20; // Number of records per page
   const offset = (page - 1) * pageSize; // Calculate the offset
 
   let query;
-  if (searchValue && searchColumn) {
-    query = `SELECT * FROM WHITE_DATA WHERE ${searchColumn} LIKE '%${searchValue}%' LIMIT ${offset}, ${pageSize}`;
+  if (searchData) {
+    searchData = JSON.parse(searchData);
+    searchQuery = '';
+    if (Object.keys(searchData).length === 1) {
+      Object.keys(searchData).forEach((searchDataO) => {
+        searchQuery =
+          searchQuery + `${searchDataO} LIKE '%${searchData[searchDataO]}%'`;
+      });
+    } else {
+      Object.keys(searchData).forEach((searchDataO, index) => {
+        if (index === Object.keys(searchData).length - 1) {
+          searchQuery =
+            searchQuery + `${searchDataO} LIKE '%${searchData[searchDataO]}%'`;
+        } else {
+          searchQuery =
+            searchQuery +
+            `${searchDataO} LIKE '%${searchData[searchDataO]}%' AND `;
+        }
+      });
+    }
+    query = `SELECT * FROM WHITE_DATA_TABLE WHERE ${searchQuery} LIMIT ${offset}, ${pageSize}`;
   } else {
-    query = `SELECT * FROM WHITE_DATA LIMIT ${offset}, ${pageSize}`;
+    query = `SELECT * FROM WHITE_DATA_TABLE LIMIT ${offset}, ${pageSize}`;
   }
 
+  db.query(query, async (err, results) => {
+    if (err) {
+      console.error('Database query error: ' + err.message);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      res.status(200).json({
+        data: results,
+      });
+    }
+  });
+});
+
+app.get('/api/data/rows', (req, res) => {
+  const query = `SELECT COUNT(*) as total FROM WHITE_DATA_TABLE`;
   db.query(query, (err, results) => {
     if (err) {
       console.error('Database query error: ' + err.message);
@@ -48,14 +82,61 @@ app.get('/api/data', (req, res) => {
   });
 });
 
-app.get('/api/data/rows', (req, res) => {
-  const query = `SELECT COUNT(*) as total FROM WHITE_DATA`;
+app.get('/api/data/rows/data', (req, res) => {
+  let searchData = searchDataForOtherQuery;
+  let query;
+  if (searchData) {
+    searchData = JSON.parse(searchData);
+    searchQuery = '';
+    if (Object.keys(searchData).length === 1) {
+      Object.keys(searchData).forEach((searchDataO) => {
+        searchQuery =
+          searchQuery + `${searchDataO} LIKE '%${searchData[searchDataO]}%'`;
+      });
+    } else {
+      Object.keys(searchData).forEach((searchDataO, index) => {
+        if (index === Object.keys(searchData).length - 1) {
+          searchQuery =
+            searchQuery + `${searchDataO} LIKE '%${searchData[searchDataO]}%'`;
+        } else {
+          searchQuery =
+            searchQuery +
+            `${searchDataO} LIKE '%${searchData[searchDataO]}%' AND `;
+        }
+      });
+    }
+    query = `SELECT COUNT(*) as total FROM WHITE_DATA_TABLE WHERE ${searchQuery}`;
+  } else {
+    query = `SELECT COUNT(*) as total FROM WHITE_DATA_TABLE`;
+  }
+
   db.query(query, (err, results) => {
     if (err) {
-      console.error('Database query error: ' + err.message);
       res.status(500).json({ error: 'Internal server error' });
     } else {
       res.status(200).json(results);
+    }
+  });
+});
+
+app.get('/login/:email/:password', (req, res) => {
+  const email = req.params.email;
+  const password = req.params.password;
+  const query = `SELECT * FROM user_db WHERE email='${email}' AND password='${password}'`;
+
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      if (results.length == 0) {
+        res.status(400).json({
+          status: false,
+        });
+      } else {
+        res.status(200).json({
+          status: true,
+        });
+      }
     }
   });
 });
